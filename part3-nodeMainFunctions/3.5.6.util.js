18c6f3b8bb324c0b1e04f4d7afb8020c1a84e322
Jengyoung <young_pallete@naver.com>
2021-03-29 11:14:10 +0900

    3.5.6. util.promisify()

---
+++ part3-nodeMainFunctions/3.5.6.util.js
@@ -0,0 +1,10 @@
+const util = require('util');
+const crypto = require('crypto');
+const randomBytesPromise = util.promisify(crypto.randomBytes);
+randomBytesPromise(64)
+    .then((buf) => {
+        console.log(buf.toString('base64'));
+    })
+    .catch((error) => {
+        console.error(error);
+    });
