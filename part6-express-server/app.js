72915f984a0d70976ab32ed1711ef93c8c81cc21
JengYoung <young_pallete@naver.com>
2022-01-31 15:23:24 +0900

    feat: multer image upload

--- part6-express-server/app.js
+++ part6-express-server/app.js
@@ -50,6 +50,44 @@ app
     next();
   })
   .use(cookieParser("secretKey..."));
+
+// multer middleware
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (e) {
+  console.error("create uploads directory");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+app.post(
+  "/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  }
+);
+
 app.get(
   "/",
   (req, res, next) => {
